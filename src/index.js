99205fae5a7d698934735e3ab3f834638feac090
nexorqk <pkms2a10@gmail.com>
2024-09-24 18:11:23 +0300

    feat: working solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const lastItem = stack[stack.length - 1];
+    const currentItem = str[i];
+    let configTypeIndex = 0;
+    bracketsConfig.forEach((item, index) =>
+      item.indexOf(currentItem) !== -1 ? (configTypeIndex = index) : null
+    );
+
+    const openBracket = bracketsConfig[configTypeIndex][0];
+    const closeBracket = bracketsConfig[configTypeIndex][1];
+    const isEqualBrackets = openBracket === closeBracket;
+    if (stack.length > 0) {
+      if (isEqualBrackets) {
+        if (lastItem === currentItem) {
+          stack.pop();
+        } else {
+          stack.push(currentItem);
+        }
+      } else {
+        if (currentItem === openBracket) {
+          stack.push(currentItem);
+        }
+        if (currentItem === closeBracket && lastItem === openBracket) {
+          stack.pop();
+        }
+      }
+    } else {
+      if (closeBracket === currentItem && !isEqualBrackets) {
+        return false;
+      } else {
+        stack.push(currentItem);
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
